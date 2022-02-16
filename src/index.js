896acea19347cf1675ed239483ef87abda40c027
Natalya Stepanova <natalyastepanova@MacBook-Air-Natalya.local>
2022-02-16 22:54:31 +0300

    feat: solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,33 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    const exprDec = expr.match(/.{1,10}/g);
+    const exprDel = exprDec.map(item => item.replace(/0/g, ' ').trimStart().replace(/ /g, '0'));
+    const exprMorse = exprDel.map(item => {
+        let newItem = '';
+        if (item == '**********') {
+            newItem = ' ';
+        } else {  
+            for (let i=1; i <item.length; i = i +2) {
+                if (item[i] == '0') {
+                    newItem += '.'
+                } else if (item[i] == '1') {
+                     newItem += '-'
+                }
+            }
+        }
+    return newItem;
+    })
+    const result = exprMorse.map (item => {
+        for (let key in MORSE_TABLE) {
+          if (item == key) {
+            return MORSE_TABLE[key];
+          } else if (item == ' ')
+            return item;
+        }
+    })
+    return result.join('');
+
 }
 
 module.exports = {
